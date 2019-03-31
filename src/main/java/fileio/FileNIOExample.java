a90a45569a091087d32974ea8b70e7f2ded7b3c4
m1ndas <darbas.mindaugas@gmail.com>
2019-03-31 10:51:12 +0300

    Adding Person class for parsing objects from file excercise

--- src/main/java/fileio/FileNIOExample.java
+++ src/main/java/fileio/FileNIOExample.java
@@ -4,19 +4,33 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileNIOExample {
     public static void main(String[] args){
-        //    Product product = new Product();
+
+        List<Person> myFriends = new ArrayList<>();
+
         try {
-            List<String> productLines = Files.readAllLines(Paths.get("./target/classes/input.csv"), StandardCharsets.UTF_8);
-            for (:
-                 ) {
-                
+            List<String> linesOfText = Files.readAllLines(Paths
+                    .get("./target/classes/input.csv"), StandardCharsets.UTF_8);
+
+            for(int i = 0; i < linesOfText.size(); i++){
+                if(i == 0)
+                    continue;
+
+                // System.out.println(linesOfText.get(i));
+                String[] vals = linesOfText.get(i).split(",");
+                // add a new person to a list of friends
+                myFriends.add(new Person(Integer.parseInt(vals[0]), vals[1], Integer.parseInt(vals[2])));
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        for (Person friend: myFriends) {
+            System.out.println(friend);
         }
     }
 }
---
+++ src/main/java/fileio/Person.java
@@ -0,0 +1,43 @@
+package fileio;
+
+public class Person {
+    int id, age;
+    String name;
+
+    public Person(){}
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + ", name='" + name + '\'' + ", age=" + age + '}';
+    }
+}
